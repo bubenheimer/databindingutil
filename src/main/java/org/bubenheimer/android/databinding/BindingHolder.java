e7e4cb950179d0d8ae78feed75f5aa8f91cbd010
Uli <ububen@gmail.com>
2016-12-10 14:38:05 -0600

    Initial commit

---
+++ src/main/java/org/bubenheimer/android/databinding/BindingHolder.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) 2015-2016 Uli Bubenheimer.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package org.bubenheimer.android.databinding;
+
+import android.databinding.DataBindingUtil;
+import android.databinding.ViewDataBinding;
+import android.support.annotation.LayoutRes;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+
+public class BindingHolder<T extends ViewDataBinding> extends RecyclerView.ViewHolder {
+    public final T binding;
+
+    public BindingHolder(final T binding) {
+        super(binding.getRoot());
+        this.binding = binding;
+    }
+
+    public static <T extends ViewDataBinding> BindingHolder<T>
+    create(final ViewGroup parent, final @LayoutRes int layoutId) {
+        final T binding = DataBindingUtil.inflate(LayoutInflater.from(parent.getContext()),
+                layoutId, parent, false);
+        return new BindingHolder<>(binding);
+    }
+}
